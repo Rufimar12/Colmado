05747f923c293a220c93fa3e1bbdb85bcc3a309a
Rufimar12 <rufi0121.euphoria@gmail.com>
2023-03-03 17:52:15 -0400

    definicion de modelos

---
+++ Server/Models/UsuarioRol.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Colmado.Server.Models;
+
+public class UsuarioRol
+{
+   [Key]
+    public int Id { get; set; }
+   public string Nombre { get; set; }=null!;
+   public bool PermisoParaCrear { get; set; }
+   public bool PermisoParaEditar { get; set; }
+   public bool PermisoParaEliminar { get; set; }
+}
