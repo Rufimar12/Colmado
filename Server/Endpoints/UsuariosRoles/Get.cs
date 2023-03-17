36c57014ee01c4799c47b71578ea7c884c131dcb
Rufimar12 <rufi0121.euphoria@gmail.com>
2023-03-17 17:32:17 -0400

    get y result list

---
+++ Server/Endpoints/UsuariosRoles/Get.cs
@@ -0,0 +1,27 @@
+using Ardalis.ApiEndpoints;
+using Colmado.Server.Context;
+using Colmado.Shared.Records;
+ using Colmado.Shared.Wrapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Colmado.Server.Endpoints.UsuariosRoles;
+using Respuesta = ResultList<UsuarioRolRecord>;
+
+public class Get : EndpointBaseAsync.WithoutRequest.WithActionResult<Respuesta>
+{
+    private readonly IMyDbContext dbContext;
+
+    public Get(IMyDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    public override async Task<ActionResult<Respuesta>> HandleAsync(CancellationToken cancellationToken = default)
+    {
+        var roles = await dbContext.UsuariosRoles
+        .Select(rol=>rol.ToRecord())
+        .ToListAsync(cancellationToken);
+        
+        return Respuesta.Success(roles);
+    }
+}
