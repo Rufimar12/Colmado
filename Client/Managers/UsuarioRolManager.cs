de8c667f1a6ea03f9151b985c35836df1b53f373
Rufimar12 <rufi0121.euphoria@gmail.com>
2023-03-17 20:02:16 -0400

    client

---
+++ Client/Managers/UsuarioRolManager.cs
@@ -0,0 +1,29 @@
+using Colmado.Shared.Wrapper;
+using Colmado.Shared.Records;
+using Colmado.Shared.Routes;
+using Colmado.Client.Extensions;
+
+namespace Colmado.Client.Managers;
+
+public class UsuarioRolManager 
+{
+    private readonly HttpClient httpClient;
+
+    public UsuarioRolManager(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task<ResultList<UsuarioRolRecord>> GetAsync()
+    {
+        try{
+             var response = await httpClient.GetAsync(UsuarioRolRouteManager.BASE);
+             var resultado = await response.ToResultList<UsuarioRolRecord>();
+             return resultado;
+        }
+        catch(Exception e){
+            return ResultList<UsuarioRolRecord>.Fail(e.Message);
+        }
+    }
+
+}
